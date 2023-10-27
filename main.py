b61c511311fbab38a1c51a5c7c78d16ec8b2f0b3
WarmMilkCodes <87686876+WarmMilkCodes@users.noreply.github.com>
2023-10-27 11:33:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pyglet
 from pyglet.gl import *
 from engine.world import World
+from pyglet.gl import gluLookAt
 
 win = pyglet.window.Window(800, 600, "Voxel Engine")
 world = World()
+
+chunk_position = [0, 0, 0]
+camera_position = [CHUNK_SIZE / 2, CHUNK_SIZE / 2, -CHUNK_SIZE * 1.5]
 
 def draw_voxel(x, y, z, color=(1, 1, 1) size=1):
     vertices = [
@@ -38,5 +42,26 @@ def draw_chuk(chunk, start_x=0, start_y=0, start_z=0):
 def on_draw():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glEnable(GL_DEPTH_TEST)
-    
+
+    # Setup rudimentary camera
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(60, win.width/win.height, 0.1, 1000)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    gluLookAt(*camera_position, CHUNK_SIZE/2, CHUNK_SIZE/2, CHUNK_SIZE/2, 0, 1, 0)
+
+    draw_chunk(world.get_chunk(0, 0, 0), *chunk_position)
     draw_voxel(0, 0, 0)
+
+@win.event
+def on_key_press(symbol, modifiers):
+    step = 1
+    if symbol == pyglet.window.key.RIGHT:
+        chunk_position[0] += step
+    elif symbol == pyglet.window.key.LEFT:
+        chunk_position[0] -= step
+    elif symbol == pyglet.window.key.UP:
+        chunk_position[1] += step
+    elif symbol == pyglet.window.key.DOWN:
+        chunk_position[1] -= step
