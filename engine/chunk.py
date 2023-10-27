5d6164806c3c0f7930e65683a05507c0b302af3e
WarmMilkCodes <87686876+WarmMilkCodes@users.noreply.github.com>
2023-10-27 11:27:36 -0500

    Update chunk.py

--- engine/chunk.py
+++ engine/chunk.py
@@ -1,8 +1,9 @@
+import random
+
 CHUNK_SIZE = 16
 
 class Chunk:
   def __init__(self):
     # Initialize a 3D array of None values
-    # None = empty voxel
-    # Replace None with actual voxel data later
-    self.voxels = [[[None for _ in range(CHUNK_SIZE) for _ in range(CHUNK_SIZE)] for _ in range(CHUNK_SIZE)]
+    self.voxels = [[[random.choice([(1, 0, 0), (0, 1, 0), (0, 0, 1), None]) for _ in range(CHUNK_SIZE)]
+                        for _ in range(CHUNK_SIZE)] for _ in range(CHUNK_SIZE)]
